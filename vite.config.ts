127f604d7552ff439a94a13c08febb5544b31d84
Rorisang Makgana <rorisangmakganaiv@gmail.com>
2025-12-08 23:06:09 +0200

    Resolving PWA Issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -19,6 +20,7 @@ export default defineConfig(({ mode }) => {
     },
 
     plugins: [
+      commonjs(),
       react(),
       VitePWA({
         registerType: 'autoUpdate',
@@ -119,8 +121,13 @@ export default defineConfig(({ mode }) => {
             vendor: ['react', 'react-dom', 'react-router-dom'],
           },
         },
+        external: [], // Ensure nothing is marked as external
       },
       copyPublicDir: true,
+      // Add this to handle CommonJS modules
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
     },
 
     publicDir: 'public',
